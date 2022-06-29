f161cfea25cd89eb4e4beeca49d677dfc53f215b
Pinto Manuel <pintoydc@gmail.com>
2022-06-29 09:48:07 +0200

    Code commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,8 @@
-import { Box, Container, Grid, ListItem } from '@mui/material';
-import * as React from 'react';
+import { Box, Container, Grid } from '@mui/material';
 import BtnStart from '../button/BtnStart';
 import Card from '../card/Card';
-import WeatherImg from './../../assets/weather.png'
-
-
+import WeatherImg from './../../assets/weather.png';
+import { motion } from 'framer-motion';
 
 const Header = () => {
 	return (
@@ -16,21 +14,23 @@ const Header = () => {
 							<h1 className='mb-4 mt-4 text-purple font-extrabold'>PRO UPDATES ON TIME </h1>
 
 							<h1 className='flex font-bold flex-col text-5xl leading-none mb-20 '>
-								Weather forecasts, 
+								Weather forecasts,
 								<span>nowcasts.</span>
 								<BtnStart />
 							</h1>
 						</div>
 
-						<div className='grow -z-50'>
-							<img src={WeatherImg} alt="" />
-						</div>
+						<motion.div
+							animate={{ x: [ 0, 100, 0 ] }}
+							transition={{ ease: 'easeOut', duration: 2 }}
+							className='grow -z-50'>
+							<img src={WeatherImg} alt='' />
+						</motion.div>
 					</Box>
 					{/* Card Component */}
 					<Card />
 				</Box>
 			</Grid>
-			
 		</Container>
 	);
 };
